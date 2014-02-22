c2b6927d2c89a6e419aa10c9d1cecc3c537759b7
TaniaG <crunchycandycane@unknown7056818ceb27.gateway.2wire.net>
2014-02-21 18:09:39 -0800

    Update

---
+++ guessinggame.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{  int x;
+  	int guess;
+	
+	x = 55;
+	
+
+	cout << "What is your guess?"  << endl;
+	cin >> guess;
+
+	if (guess < x)
+	cout << "Too low!"  << endl;
+
+	else if (guess > x)
+	cout << "Too high!" << endl;
+
+	else 
+	cout << "That's right! Good job!" << endl;
+
+	return 0;
+
+}
--- gyr2.cpp
+++ gyr2.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main ()  
-{      bool status;
-       string color1 ;
+{      
+   string color1 ;
 
 	cout << "What is your color?" << endl;
 	cin >> color1;
@@ -14,26 +13,24 @@ int main ()
 	 if
 	 (color1 == "green") 
 	 		{
-	 			status = true;
+	 		
 	 			cout << "You are doing good!" << endl;
 	 		}
 
-	if
+	else if
 	 (color1 == "yellow") 
-		    {
-		    	status = true;
-		    cout << "You are doing okay." << endl;
-		    }
-
-	if 
+		   
+		   		    cout << "You are doing okay." << endl;
+		    
+	else if 
 		(color1 == "red") 
-			{
-				status = true;
+			
+			
 			cout << "You are doing poorly." << endl;
-			} 
+			
 
 	else 
-		status = false;
+		
 		cout << "Status unavailable. Enter valid string." << endl; 
 
 	return 0;
